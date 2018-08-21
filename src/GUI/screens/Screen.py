ad29dd56f3889bc4bf998164c7a955ad45da3329
Helldragger <christopher1223334444@live.fr>
2018-08-21 07:19:59 +0200

    Added contextual buttons support for multi screen screens

--- src/GUI/screens/Screen.py
+++ src/GUI/screens/Screen.py
@@ -2,9 +2,9 @@ from abc import abstractmethod
 
 from discord import Embed, File, Message
 
+import pipeline.Processing as Pr
 from pipeline.EventType import EventType, getEmojiEvent
 from pipeline.Listener import Listener
-import pipeline.Processing as Pr
 
 
 class Screen(Listener):
@@ -18,6 +18,7 @@ class Screen(Listener):
     deleted: bool
     edited: bool
     msg_ref: Message
+    contextual_buttons: [EventType]
 
     @abstractmethod
     def __init__(self,emoji_buttons:[EventType], watched_events: [
@@ -33,7 +34,7 @@ class Screen(Listener):
         self.files = None
         self.delete_after = None
         self.emoji_buttons = emoji_buttons
-
+        self.contextual_buttons = []
 
 
     async def send(self, context):
@@ -48,27 +49,33 @@ class Screen(Listener):
             # register the listener
             for ev in self.emoji_buttons:
                 if getEmojiEvent(ev.value) != EventType.UNKNOWN_EMOJI:
-                    self.registerButton(ev)
+                    await self.registerButton(ev)
                 else:
                     Pr.registerScreen(self, ev)
+            for button in self.contextual_buttons:
+                await self.registerButton(button)
 
 
 
     async def registerButton(self, emojiButton):
-        Pr.registerScreenButton(self, emojiButton)
         await self.msg_ref.add_reaction(emojiButton.value)
+        Pr.registerScreenButton(self, emojiButton)
 
     async def unregisterButton(self, emojiButton):
-        Pr.unregisterScreenButton(self, emojiButton)
-        await self.msg_ref.remove_reaction(emojiButton.value, self.msg_ref.author)
+        await self.msg_ref.remove_reaction(emojiButton.value,
+                                           self.msg_ref.author)  # Pr.unregisterScreenButton(self, emojiButton)
 
 
     async def updateScreen(self):
         if self.sent and not self.deleted:
+            for button in self.contextual_buttons:
+                await self.unregisterButton(button)
             self.build()
             await self.msg_ref.edit(content='\n'.join(self.content),
                                     embed=self.embed,
                                     delete_after=self.delete_after)
+            for button in self.contextual_buttons:
+                await self.registerButton(button)
 
     async def delete(self):
         if self.sent and not self.deleted:
@@ -81,4 +88,5 @@ class Screen(Listener):
 
     @abstractmethod
     def build(self):
+        self.content = ["SAMPLE SCREEN, NOT READY YET"]
         pass
